0b5be1a744475ef18241e7e9a382b4906ddf80c9
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-04 06:23:13 -0700

    chore: Day4-0

--- Day4-CampCleanup/Program.cs
+++ Day4-CampCleanup/Program.cs
@@ -1,13 +1,1 @@
-﻿string testRanges = "2-4,6-8";
-Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
-testRanges = "2-3,4-5";
-Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
-testRanges = "27-32,12-42";
-Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
-
-(Range, Range) ParseRanges(string ranges)
-{
-    return (new Range(0, 0), new Range(0, 0));
-}
-
-record Range(int Start, int End);
+﻿
