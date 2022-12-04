83ffd60b6d9e1161785b65141a73c5ca1449c7d3
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-04 06:30:37 -0700

    chore: Add test for Contains

--- Day4-CampCleanup/Program.cs
+++ Day4-CampCleanup/Program.cs
@@ -1,9 +1,26 @@
 ﻿string testRanges = "2-4,6-8";
+Console.WriteLine("Test Parse:");
 Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
 testRanges = "2-3,4-5";
 Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
 testRanges = "27-32,12-42";
 Console.WriteLine($"{testRanges} => {ParseRanges(testRanges)}");
+
+Console.WriteLine("Test Contain");
+Range first = new Range(2, 4);
+Range second = new Range(6, 8);
+Console.WriteLine($"{first} contains {second}: {Contains(first, second)}");
+first = new Range(2, 8);
+second = new Range(3, 7);
+Console.WriteLine($"{first} contains {second}: {Contains(first, second)}");
+first = new Range(6, 6);
+second = new Range(4, 6);
+Console.WriteLine($"{first} contains {second}: {Contains(first, second)}");
+
+bool Contains(Range range, Range other)
+{
+    return false;
+}
 
 (Range, Range) ParseRanges(string ranges)
 {
