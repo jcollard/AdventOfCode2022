51a401cb27d2c515150b59908365de6c6313caab
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-06 05:25:55 -0700

    chore: Complete Day 6.

---
+++ Day6-TuningTrouble/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = File.ReadAllText("input.txt");
+FindWindow(input, 14);
+int FindWindow(string toScan, int windowSize)
+{
+    for (int i = 0; i < toScan.Length - windowSize; i++)
+    {
+        string windowed = toScan[i..(i+windowSize)];
+        if (windowed.ToHashSet().Count() == windowSize)
+        {
+            Console.WriteLine($"{windowed} @ {i} -> {i+windowSize}");
+            break;
+        }
+    }
+    return 0;
+}
