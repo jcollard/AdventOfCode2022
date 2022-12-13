64e9d9857c80994f15fae17b201ba0fb86c1e82b
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-13 09:03:28 -0700

    feat: Implement Part2.

--- Day13/Solution/Program.cs
+++ Day13/Solution/Program.cs
@@ -1,18 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string data = File.ReadAllText("input.txt");
-string[] pairs = data.Split("\n\n");
-int ix = 1;
-int sum = 0;
-foreach (string pair in pairs)
+string data = File.ReadAllText(args[0]);
+
+Part2();
+
+void Part2()
 {
-    string[] packetStrings = pair.Split("\n");
-    Packet p0 = Packet.Parse(packetStrings[0].Trim());
-    Packet p1 = Packet.Parse(packetStrings[1].Trim());
-    // Console.WriteLine($"{p0} vs {p1}");
-    if (p0.Compare(p1) <= 0)
-    {
-        sum += ix;
-    }
-    ix++;
+    List<Packet> packets = data
+        .Split("\n")
+        .Where(s => s.Trim() != string.Empty)
+        .Select(s => s.Trim())
+        .Select(Packet.Parse).ToList();
+
+    Packet marker0 = Packet.Parse("[[2]]");
+    Packet marker1 = Packet.Parse("[[6]]");
+    packets.Add(marker0);
+    packets.Add(marker1);
+    packets.Sort((p0, p1) => p0.Compare(p1));
+    int ix0 = packets.IndexOf(marker0) + 1;
+    int ix1 = packets.IndexOf(marker1) + 1;
+    Console.WriteLine($"{marker0} found at {ix0} and {marker1} found at {ix1}");
+    Console.WriteLine($"Decoder key is {ix0 * ix1}");
 }
-Console.WriteLine(sum);
+
+void Part1()
+{
+    string[] pairs = data.Split("\n\n");
+    int ix = 1;
+    int sum = 0;
+    foreach (string pair in pairs)
+    {
+        string[] packetStrings = pair.Split("\n");
+        Packet p0 = Packet.Parse(packetStrings[0].Trim());
+        Packet p1 = Packet.Parse(packetStrings[1].Trim());
+        // Console.WriteLine($"{p0} vs {p1}");
+        if (p0.Compare(p1) <= 0)
+        {
+            sum += ix;
+        }
+        ix++;
+    }
+    Console.WriteLine(sum);
+}
